b8256c67a5db3f58d0ca4522d14eb1dde03d7316
Kien Pang <kienpang@gmail.com>
2016-08-02 10:05:47 +0200

    Simplify onClick by moving it to xml - based on feedback

--- app/src/main/java/com/tinytinybites/myappportfolio/MainActivity.java
+++ app/src/main/java/com/tinytinybites/myappportfolio/MainActivity.java
@@ -6,69 +6,23 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity{
 
-
-    //Ui Elements
-    private TextView mProjectPopularMoviesTv;
-    private TextView mProjectStockHawkTv;
-    private TextView mProjectBuildItBiggerTv;
-    private TextView mProjectMaterialTv;
-    private TextView mProjectGoUbiquitousTv;
-    private TextView mProjectCapstoneTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mProjectPopularMoviesTv = (TextView) findViewById(R.id.projectPopularMovies);
-        mProjectStockHawkTv = (TextView) findViewById(R.id.projectStockHawk);
-        mProjectBuildItBiggerTv = (TextView) findViewById(R.id.projectBuildItBigger);
-        mProjectMaterialTv = (TextView) findViewById(R.id.projectMaterial);
-        mProjectGoUbiquitousTv = (TextView) findViewById(R.id.projectGoUbiquitous);
-        mProjectCapstoneTv = (TextView) findViewById(R.id.projectCapstone);
-
-        //Set On click
-        mProjectPopularMoviesTv.setOnClickListener(this);
-        mProjectStockHawkTv.setOnClickListener(this);
-        mProjectBuildItBiggerTv.setOnClickListener(this);
-        mProjectMaterialTv.setOnClickListener(this);
-        mProjectGoUbiquitousTv.setOnClickListener(this);
-        mProjectCapstoneTv.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        String appName = null;
-
-        switch(v.getId()){
-            case R.id.projectBuildItBigger:{
-                appName = getString(R.string.application_listing_project_build_it_bigger);
-                break;
-            }
-            case R.id.projectCapstone:{
-                appName = getString(R.string.application_listing_project_capstone);
-                break;
-            }
-            case R.id.projectGoUbiquitous:{
-                appName = getString(R.string.application_listing_project_go_ubiquitous);
-                break;
-            }
-            case R.id.projectMaterial:{
-                appName = getString(R.string.application_listing_project_make_your_app_material);
-                break;
-            }
-            case R.id.projectPopularMovies:{
-                appName = getString(R.string.application_listing_project_popular_movies);
-                break;
-            }
-            case R.id.projectStockHawk:{
-                appName = getString(R.string.application_listing_project_stock_hawk);
-                break;
-            }
+    public void projectClicked(View v) {
+        //Double check
+        if(!(v instanceof TextView)){
+            //Do nothing
+            return;
         }
 
+        String appName = ((TextView)v).getText().toString();
         if(appName != null){
             Toast.makeText(MainActivity.this, String.format(getString(R.string.application_listing_button_toast), appName), Toast.LENGTH_SHORT).show();
         }
